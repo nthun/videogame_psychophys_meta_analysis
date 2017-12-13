75030df76c5c98e6b383e16066ab1cc142be7fd8
Tamas Nagy <thomassocska@yahoo.com>
2017-12-14 00:58:40 +0100

    Add script for merging records and removing duplicates

--- script/merge_records.R
+++ script/merge_records.R
@@ -1,3 +1,58 @@
 # Merge records, remove duplicates
 library(tidyverse)
 library(openxlsx)
+
+
+# Read all data sources ---------------------------------------------------
+scopus <- read_csv("screening_data/scopus_raw.csv") %>% 
+    select(source = Source,
+           doi = DOI,
+           pmid = `PubMed ID`,
+           eid = EID,
+           title = Title,
+           journal = `Source title`,
+           authors = Authors,
+           year = Year,
+           abstract = Abstract,
+           language = `Language of Original Document`,
+           type = `Document Type`
+           )
+
+pubmed <- read.xlsx("screening_data/pubmed_raw.xlsx", 1) %>% 
+            as_tibble() %>% 
+            select(source,
+                   doi,
+                   pmid,
+                   title,
+                   journal,
+                   year,
+                   authors,
+                   abstract)
+
+
+proquest_dt <- read_csv("screening_data/proquest_df.csv") %>% 
+               select(source,
+                      pq_id,
+                      title,
+                      type,
+                      level,
+                      year,
+                      authors = author,
+                      abstract
+                      )
+
+anderson2010 <- read_csv("screening_data/anderson2010.csv")
+
+# Removing duplicates based on title, doi, pmid
+merged_records <-
+    bind_rows(scopus, 
+              pubmed, 
+              proquest_dt, 
+              anderson2010) %>% 
+    mutate(title = title %>% str_to_title()) %>% 
+    filter(!(
+            duplicated(doi, incomparables = NA) |
+            duplicated(pmid, incomparables = NA) |
+            duplicated(title, incomparables = NA))) %>%
+    arrange(title)
+    
