67701d1cd78542e1dfbc47e4c7d236ac6b6d0c3b
Tamas Nagy <thomassocska@yahoo.com>
2017-12-20 12:52:04 +0100

    Add possibility to define id_hierarchy manually. Extended documentation

--- script/functions/make_id.R
+++ script/functions/make_id.R
@@ -1,14 +1,14 @@
 # Select the best id based on a hierarchy, and keep only that for the article
 # INPUT: A data frame that has identifiers 
+#        id_hierarchy: a data frame with identifier<chr>, and rank<num> columns that define the rank (importance) of an identifier. Smaller number means more important
 # OUTPUT: a data frame without individual id columns, and with new identifier and id columns that contain the best available identifier and the id, respectively
 # EXAMPLE: make_id(df)
-
 library(tidyr)
 library(dplyr)
 id_hierarchy <- tibble(identifier = c("doi","pmid","psyid","eid","pq_id","no_id"),
                        id_rank = c(1, 2, 3, 4, 5, 6))
 
-make_id <- function(df){
+make_id <- function(df, id_hierarchy = id_hierarchy){
     # Stop if there are no valid identifiers in the data frame
     stopifnot(names(df) %>% 
                   intersect(id_hierarchy$identifier) %>% 
