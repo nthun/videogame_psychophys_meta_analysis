c77e01bf32fb60d924cdd82d69b1e161f19e145f
Tamas Nagy <thomassocska@yahoo.com>
2017-12-20 16:35:07 +0100

    Add libraries, correct bug in directory creation.

--- script/functions/save_locally.R
+++ script/functions/save_locally.R
@@ -2,19 +2,24 @@
 # iNPUT: df: a dataset that contains the assigned articles, and having a reviewer<chr> column
 # OUTPUT: No output, only side effect (saved files in the folder)
 # EXAMPLE: save_locally(assigned_articles, "screening")
-save_locally <- function(df, save_dir = NA_character_, overwrite = FALSE){
+library(dplyr)
+library(readr)
+library(tidyr)
+library(glue)
+
+save_locally <- function(df, dir = NA_character_, overwrite = FALSE){
     # Checking predicaments
     stopifnot(has_name(df, "reviewer"),
-              !is.na(save_dir),
-              !(dir.exists(save_dir) & overwrite == FALSE))
+              !is.na(dir),
+              !(dir.exists(dir) & overwrite == FALSE))
     # Create a nested tibble 
     df_nested <- 
         df %>% 
         group_by(reviewer) %>% 
         nest()
     # Create the directory
-    if (dir.exists(save_dir) & overwrite == TRUE) dir.create(save_dir)
-    if (!dir.exists(save_dir)) dir.create(save_dir)
+    if (dir.exists(dir) & overwrite == TRUE) unlink(dir, recursive = TRUE)
+    dir.create(dir)
     # Save the screening filess
     walk2(df_nested$reviewer, df_nested$data, ~write_excel_csv(.y, glue("{dir}/{.x}_articles.csv"), na = ""))
     glue("{nrow(df_nested)} files saved in {dir}")
