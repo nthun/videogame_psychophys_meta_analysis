785f1a7ac2d690a7d32dd4c84045f20be7f534e6
Tamas Nagy <thomassocska@yahoo.com>
2018-01-03 15:50:59 +0100

    Add create consensus function

---
+++ script/functions/create_consensus.R
@@ -0,0 +1,47 @@
+# Create consensus tables
+# iNPUT: articles: a dataset that contains the screened articles, and having a name<chr> column
+# OUTPUT: A nested data frame containing the mutual records for all pairs of screning team members
+# EXAMPLE: create_consensus(articles)
+library(dplyr)
+library(tidyr)
+library(purrr)
+
+create_consensus <- function(artciles){
+    name_pairs <- 
+        articles %>% 
+        distinct(name) %>% 
+        pull() %>% 
+        combn(., 2) %>% 
+        t() %>% 
+        as_data_frame() %>% 
+        set_names(c("name1", "name2")) %>% 
+        mutate(name_pair = paste(name1, name2, sep = "_")) %>% 
+        group_by(name_pair) %>% 
+        nest()
+    
+        name_pairs %>% 
+        mutate(decision_table = map(data, 
+                                    ~ articles %>% 
+                                        drop_na(decision) %>%
+                                        select(-position, -reason) %>%
+                                        filter(name %in% (.x %>% c())) %>% 
+                                        spread(name, decision, sep = "_") %>% 
+                                        set_names(str_replace(names(.), "name", "decision")) %>% 
+                                        drop_na(contains("decision"))),
+               reason_table = map(data, 
+                                  ~ articles %>% 
+                                      drop_na(decision) %>%
+                                      select(-position, -decision) %>%
+                                      filter(name %in% (.x %>% c())) %>% 
+                                      spread(name, reason, sep = "_") %>% 
+                                      set_names(str_replace(names(.), "name", "reason")))) %>% 
+        transmute(
+            name_pair, 
+            consensus_table = map2(decision_table, reason_table, 
+                                   ~ left_join(.x, .y) %>% 
+                                       select(contains("decision"), contains("reason"), title, abstract, everything())
+            )
+        )
+}
+
+
