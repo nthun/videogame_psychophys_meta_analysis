11d8ddbf2c8ca984b32b412ec214ab02e5cf21f9
Tamas Nagy <thomassocska@yahoo.com>
2018-01-03 16:02:45 +0100

    Add saving consensus tables

---
+++ script/functions/save_consensus_locally.R
@@ -0,0 +1,20 @@
+# Save consensus tavles articles locally in a specified directory
+# iNPUT: tables: a dataset that contains the assigned articles, and having a reviewer<chr> column
+# OUTPUT: No output, only side effect (saved files in the folder)
+# EXAMPLE: save_locally(consensus_tables, "consensus_tables")
+# TODO: Change to work on unnested tables?
+library(dplyr)
+library(tidyr)
+
+save_consensus_locally <- function(consensus_tables, dir = NA_character_, overwrite = FALSE){
+    # Checking predicaments
+    stopifnot(has_name(consensus_tables, "reviewer"),
+              !is.na(dir),
+              !(dir.exists(dir) & overwrite == FALSE))
+   # Create the directory
+    if (dir.exists(dir) & overwrite == TRUE) unlink(dir, recursive = TRUE)
+    dir.create(dir)
+    # Save the screening filess
+    purrr::walk2(consensus_tables$name_pair, consensus_tables$consensus_table, ~readr::write_excel_csv(.y, glue::glue("{dir}/{.x}_consensus.csv"), na = ""))
+    glue::glue("{nrow(consensus_tables)} files saved in {dir}")
+}
