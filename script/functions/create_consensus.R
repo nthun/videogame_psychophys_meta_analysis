fe5414a036528a79d933d06ecbd073ee00a1e186
Tamas Nagy <thomassocska@yahoo.com>
2018-01-13 13:44:22 +0100

    Add consensus <int> variable to mark final consensus

--- script/functions/create_consensus.R
+++ script/functions/create_consensus.R
@@ -49,7 +49,8 @@ create_consensus <- function(articles){
             name_pair, 
             consensus_table = map2(decision_table, reason_table, 
                                    ~ left_join(.x, .y) %>% 
-                                       select(contains("decision"), contains("reason"), no_agreement, title, abstract, everything())
+                                       mutate(consensus = if_else(no_agreement == TRUE, NA_integer_, .[[1]])) %>% 
+                                       select(consensus, contains("decision"), contains("reason"), no_agreement, title, abstract, everything())
             )
         )
     )
