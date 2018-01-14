388d2c2e181163b9245c0c1d56de54225d3451d0
Tamas Nagy <thomassocska@yahoo.com>
2018-01-14 02:00:34 +0100

    Added verification of the google drive folder existence, and creates the folder if not exists

--- script/functions/upload_to_gdrive.R
+++ script/functions/upload_to_gdrive.R
@@ -2,8 +2,7 @@
 # INPUT: local_path <chr>: a full path that contains .csv-s to upload to gdrive
 #        gdrive_path <chr>: a full gdrive path, preferably with / at the end
 # OUTPUT: no output, this function exerts side-effect
-# EXAMPLE: upload_to_gdrive("D:/Documents/GitHub/videogame_psychophys_meta_analysis/temp_cons/", "Research/Video game psychophysiology meta/Literature search/temp_consensus/")
-# TODO: Verify if a gdrive folder exists (missing function), and if not make it
+# EXAMPLE: upload_to_gdrive("D:/Documents/GitHub/videogame_psychophys_meta_analysis/temp_cons/", "Research/Video game psychophysiology meta/Literature search/consensus_meetings/", overwrite = TRUE)
 
 library(dplyr)
 library(googledrive)
@@ -12,16 +11,22 @@ library(purrr)
 upload_to_gdrive <- function(local_path, gdrive_path, overwrite = FALSE){
     stopifnot(dir.exists(local_path),
               is.logical(overwrite))
-    # No check for the google drive folder, but it throws an error if not found
-    # Check if there are files in the gdrive folder
-    drive_list <- drive_ls(gdrive_path)
+
+    # Run listing safely, so if fails, does not stop the function
+    safe_drive_ls <- safely(drive_ls)
+    drive_list <- safe_drive_ls(gdrive_path)
+    
+    # If folder does not exist, create it
+    if (!is.null(drive_list$error)) drive_mkdir(gdrive_path)
     
     # If the gdrive folder exists, and contains files, and can overwrite, delete all content
-    if ((nrow(drive_list) > 0) & overwrite == TRUE) {
+    if ((nrow(drive_list$result) > 0) & overwrite == TRUE) {
         drive_trash(gdrive_path)
         drive_mkdir(gdrive_path)
     }
-    # Now that the folder is clean, upload the files    
+    
+    # Now that the folder is clean, upload the files
+    # Beware that if overwrite = FALSE, and the folder is not empty, new files may have the same name as the old ones
         walk(
             list.files(local_path, pattern = ".csv", full.names = TRUE),
             ~ drive_upload(
