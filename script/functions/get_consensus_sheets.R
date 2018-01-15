3d752ed969b75d0c550d8d919ed64f1f6dd040ab
Tamas Nagy <thomassocska@yahoo.com>
2018-01-15 18:13:11 +0100

    Added error handling to google drive listing

--- script/functions/get_consensus_sheets.R
+++ script/functions/get_consensus_sheets.R
@@ -3,7 +3,6 @@
 # OUTPUT: An unnested tibble which contains the content of all consensus files, without the decisions and the reasons.
 # EXAMPLE: get_consensus_sheets("Research/Video game psychophysiology meta/Literature search/Consensus meetings/")
 # TODO: error handling, e.g. check if all the consensus files have the right format
-# TODO: download files to a temporary directory if requested (?)
 
 library(googledrive)
 library(dplyr)
@@ -11,13 +10,25 @@ library(tidyr)
 library(purrr)
 
 get_consensus_sheets <- function(gdrive_path){
+
+    # Run listing safely, so if fails, does not stop the function
+    safe_drive_ls <- safely(drive_ls)
+    drive_list <- safe_drive_ls(gdrive_path)
     
-    drive_ls(gdrive_path) %>% 
-    transmute(file = name,
-              consensus_sheet = map(id, ~gs_key(.x) %>% 
-                                        gs_read(1))) %>% 
-    mutate(consensus_sheet = map(consensus_sheet, ~ .x %>% select(-starts_with("decision"), -starts_with("reason")))
-    ) %>% 
-    unnest(consensus_sheet)
+    # If there is an error, throw a warning
+    if (!is.null(drive_list$error)) warning("The specified google drive inventory does not exist")
+    
+    # If there are no errors in the listing, download the files
+    if (is.null(drive_list$error)){
+    
+        drive_list$result %>% 
+        transmute(file = name,
+                  consensus_sheet = map(id, ~gs_key(.x) %>% 
+                                            gs_read(1))) %>% 
+        mutate(consensus_sheet = map(consensus_sheet, 
+                                     ~ .x %>% 
+                                         select(-starts_with("decision"), -starts_with("reason")))) %>%
+        unnest(consensus_sheet)
+    }
 }
 
