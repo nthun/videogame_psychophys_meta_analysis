cb28f68eeaba8f7f96cde0c9113e12bdf3cd8a2e
Tamas Nagy <thomassocska@yahoo.com>
2018-03-11 10:35:13 +0100

    Add postfix parameter to save locally

--- script/functions/save_locally.R
+++ script/functions/save_locally.R
@@ -2,14 +2,16 @@
 # iNPUT: df: a dataset that contains the assigned articles, and having a reviewer<chr> column
 # OUTPUT: No output, only side effect (saved files in the folder)
 # EXAMPLE: save_locally(assigned_articles, "screening")
+
 library(dplyr)
 library(tidyr)
 library(tibble)
 
-save_locally <- function(df, dir = NA_character_, overwrite = FALSE){
+save_locally <- function(df, dir = NA_character_, postfix = NULL, overwrite = FALSE){
     # Checking predicaments
     stopifnot(has_name(df, "reviewer"),
               !is.na(dir),
+              !is.na(postfix),
               !(dir.exists(dir) & overwrite == FALSE))
     # Create a nested tibble 
     df_nested <- 
@@ -20,6 +22,6 @@ save_locally <- function(df, dir = NA_character_, overwrite = FALSE){
     if (dir.exists(dir) & overwrite == TRUE) unlink(dir, recursive = TRUE)
     dir.create(dir)
     # Save the screening filess
-    purrr::walk2(df_nested$reviewer, df_nested$data, ~readr::write_excel_csv(.y, glue::glue("{dir}/{.x}_articles.csv"), na = ""))
+    purrr::walk2(df_nested$reviewer, df_nested$data, ~readr::write_excel_csv(.y, glue::glue("{dir}/{.x}_{postfix}.csv"), na = ""))
     glue::glue("{nrow(df_nested)} files saved in {dir}")
 }
