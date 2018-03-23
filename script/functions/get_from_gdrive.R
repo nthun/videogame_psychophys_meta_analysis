aa9c71a3fa8e7090d01e5a24f139ad5d27f4bffe
Tamas Nagy <thomassocska@yahoo.com>
2018-03-23 22:58:09 +0100

    Add get_from_gdrive() function

---
+++ script/functions/get_from_gdrive.R
@@ -0,0 +1,31 @@
+# Get sheets from google drive
+
+# A generic function to get all spreadsheets in a gdrive and collapse them into one object
+# INPUT: A google drive path where the sheets are stored
+# OUTPUT: An unnested tibble which contains the content of all files.
+# EXAMPLE: get_from_gdrive("Research/Video game psychophysiology meta/Literature search/Consensus meetings/")
+# TODO: error handling: check if all files are google sheets
+# TODO: data validation, e.g. check if all the files have the right format
+
+library(dplyr)
+
+get_from_gdrive <- function(gdrive_path){
+    
+    # Run listing safely, so if fails, does not stop the function
+    safe_drive_ls <- purrr::safely(googledrive::drive_ls)
+    drive_list <- safe_drive_ls(gdrive_path)
+    
+    # If there is an error, throw a warning
+    if (!is.null(drive_list$error)) warning("The specified google drive inventory does not exist")
+    
+    # If there are no errors in the listing, download the files
+    if (is.null(drive_list$error)){
+        
+        drive_list$result %>% 
+            transmute(file = name,
+                      sheet = map(id, ~googlesheets::gs_key(.x) %>% googlesheets::gs_read(1))) %>% 
+            tidyr::unnest(sheet)
+    }
+}
+
+
